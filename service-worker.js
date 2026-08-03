d8c7834dfff2c1b997fe8e29da7e3bd3dcb11a97
mstpln <mattias.bjerstedt@gmail.com>
2026-08-03 19:10:26 +0200

    Synchronize v82 service-worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
 'use strict';
 importScripts('./version.js');
-const CACHE_NAME_LITERAL = 'v81';
+const CACHE_NAME_LITERAL = 'v82';
+// Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v81'.
 // Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v80'.
 // Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v79'.
 // Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v78'.
@@ -28,6 +29,7 @@ const SHELL_FILES = [
   './listeningInsightsV81.js',
   './listeningV81BootFix.js',
   './listeningV81ReviewFix.js',
+  './listeningV82Corrections.js',
   './dataLib.js',
   './listeningStats.js',
   './listeningStatsV81.js',
