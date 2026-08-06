61d204f5e8d89a8104983efa733825f0a0e521ba
mstpln <mattias.bjerstedt@gmail.com>
2026-08-06 12:11:14 +0200

    Synchronize v97 service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
 'use strict';
 importScripts('./version.js');
-const CACHE_NAME_LITERAL = 'v96';
+const CACHE_NAME_LITERAL = 'v97';
+// Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v96'.
 // Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v95'.
 // Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v94'.
 // Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v93'.
