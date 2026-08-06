7102af1015c554be6ca50b1ba27d7549b02fae58
mstpln <mattias.bjerstedt@gmail.com>
2026-08-06 13:27:35 +0200

    Bump service worker cache to v98

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
 'use strict';
 importScripts('./version.js');
-const CACHE_NAME_LITERAL = 'v97';
+const CACHE_NAME_LITERAL = 'v98';
+// Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v97'.
 // Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v96'.
 // Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v95'.
 // Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v94'.
