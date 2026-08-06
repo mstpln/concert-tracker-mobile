49a447326363fade43d76fe4267c98f5c045a4a3
mstpln <mattias.bjerstedt@gmail.com>
2026-08-06 10:37:49 +0200

    Restore service worker QA cache contract

--- service-worker.js
+++ service-worker.js
@@ -40,7 +40,7 @@ self.addEventListener('install', (event) => {
 });
 self.addEventListener('message', (event) => { if (event.data?.type === 'SKIP_WAITING') self.skipWaiting(); });
 self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key.startsWith('concert-tracker-shell-') && key !== CACHE_NAME).map((key) => caches.delete(key)))));
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k.startsWith('concert-tracker-shell-') && k !== CACHE_NAME).map((k) => caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener('fetch', (event) => {
