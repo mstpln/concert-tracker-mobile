6ce95f7fdee4f2bd6a1dc431cf55d05ec9d0c1f3
mstpln <mattias.bjerstedt@gmail.com>
2026-08-06 15:57:16 +0200

    Update v99 browser version assertions

--- playwright/listening-v85.spec.js
+++ playwright/listening-v85.spec.js
@@ -5,7 +5,7 @@ test('v98 keeps ranking semantics and aligns Band Detail listening tabs', async 
   page.on('pageerror', (error) => browserErrors.push(error.message));
   await page.goto('/');
 
-  await expect(page.locator('#start-version-refresh')).toContainText('v98');
+  await expect(page.locator('#start-version-refresh')).toContainText('v99');
 
   const teaserItems = page.locator('.stats-teaser-item');
   const traveled = teaserItems.filter({ hasText: 'traveled' });
