f9d802c58ed6c9c9edbba0d1c8b874c037a7521f
mstpln <mattias.bjerstedt@gmail.com>
2026-08-17 18:44:25 +0200

    Restore focused v138 service worker diff

--- service-worker.js
+++ service-worker.js
@@ -71,16 +71,114 @@ if (CACHE_NAME_LITERAL !== APP_VERSION) {
 }
 const CACHE_NAME = 'concert-tracker-shell-' + CACHE_NAME_LITERAL;
 const SHELL_FILES = [
-  './', './index.html', './app.css', './v72Corrections.css', './listeningV81.css', './concertCardsV86.css', './bandmarkrV87.css', './listeningReviewRollout.css', './toplistV96.css', './trustedListeningV99.css', './gau2SettingsV118.css', './settingsV123.css', './stateFeedbackV129.css', './nextConcertV138.css',
-  './app.js', './nextConcertV138.js', './interactionFeedbackV129.js', './stateFeedbackIntegrationV129.js', './providerReleaseCleanupV135.js', './gau2SettingsV118.js', './settingsV123.js', './listeningReviewRollout.js', './listeningReviewReconcile.js', './listeningCanonicalActivation.js', './listeningPreparationRecovery.js', './listeningPreparationV121.js', './gau5PreparationIntegrationV121.js', './listeningSpotifyIdentityReview.js', './listeningSpotifyIdentityReviewUi.js', './listeningIdentityPacingV105.js', './listeningIdentityCompletionV104.js', './listeningIdentityRecordingV106.js', './listeningIdentityGroupingV104.js', './devicePrivacy.js', './browserFetchPolicy.js', './v72Corrections.js', './v72FinalAdjustments.js', './securityHardening.js', './listeningInsightsV81.js', './listeningV81BootFix.js', './listeningV81ReviewFix.js', './listeningV82Corrections.js', './listeningV82GenreFix.js', './listeningV82FailSafe.js', './listeningV83ChartFix.js', './listeningV83WindowFix.js', './listeningV84ChartRenderFix.js', './listeningV85RankingAndStatsUnits.js', './toplistStatsV96.js', './toplistV96.js', './spotifyListeningMetadataV99.js', './spotifyListeningAlbumArtworkV113.js', './spotifyListeningMetadataV101.js', './trustedListeningV99.js', './uiPerformanceV126.js', './uiPerformanceV127.js', './dataLib.js', './listeningStats.js', './listeningStatsV81.js', './listeningFixtures.js', './listeningFixturesV99.js', './listeningIdentityContracts.js', './listeningDerivedStorage.js', './listeningDerivedMigration.js', './spotifyHistoryImport.js', './listeningBandActivity.js', './listeningVaultBridge.js', './listeningVault.js', './listeningHistoryV2.js', './listeningIncrementalVault.js', './listenbrainzSync.js', './spotifyHistoryBootstrap.js', './icons.js', './conflictMerge.js', './remoteStore.js', './ownedTickets.js', './musicbrainzState.js', './providerIdentityState.js', './weather.js', './spotifyUser.js', './spotifyUserV100.js', './version.js', './manifest.json', './icons/bandmarkr-wordmark.svg', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-192-maskable.png', './icons/icon-512-maskable.png', './assets/listening/album-blue.svg', './assets/listening/album-purple.svg', './assets/listening/album-cyan.svg', './assets/listening/album-gold.svg',
+  './',
+  './index.html',
+  './app.css',
+  './v72Corrections.css',
+  './listeningV81.css',
+  './concertCardsV86.css',
+  './bandmarkrV87.css',
+  './listeningReviewRollout.css',
+  './toplistV96.css',
+  './trustedListeningV99.css',
+  './gau2SettingsV118.css',
+  './settingsV123.css',
+  './stateFeedbackV129.css',
+  './nextConcertV138.css',
+  './app.js',
+  './nextConcertV138.js',
+  './interactionFeedbackV129.js',
+  './stateFeedbackIntegrationV129.js',
+  './providerReleaseCleanupV135.js',
+  './gau2SettingsV118.js',
+  './settingsV123.js',
+  './listeningReviewRollout.js',
+  './listeningReviewReconcile.js',
+  './listeningCanonicalActivation.js',
+  './listeningPreparationRecovery.js',
+  './listeningPreparationV121.js',
+  './gau5PreparationIntegrationV121.js',
+  './listeningSpotifyIdentityReview.js',
+  './listeningSpotifyIdentityReviewUi.js',
+  './listeningIdentityPacingV105.js',
+  './listeningIdentityCompletionV104.js',
+  './listeningIdentityRecordingV106.js',
+  './listeningIdentityGroupingV104.js',
+  './devicePrivacy.js',
+  './browserFetchPolicy.js',
+  './v72Corrections.js',
+  './v72FinalAdjustments.js',
+  './securityHardening.js',
+  './listeningInsightsV81.js',
+  './listeningV81BootFix.js',
+  './listeningV81ReviewFix.js',
+  './listeningV82Corrections.js',
+  './listeningV82GenreFix.js',
+  './listeningV82FailSafe.js',
+  './listeningV83ChartFix.js',
+  './listeningV83WindowFix.js',
+  './listeningV84ChartRenderFix.js',
+  './listeningV85RankingAndStatsUnits.js',
+  './toplistStatsV96.js',
+  './toplistV96.js',
+  './spotifyListeningMetadataV99.js',
+  './spotifyListeningAlbumArtworkV113.js',
+  './spotifyListeningMetadataV101.js',
+  './trustedListeningV99.js',
+  './uiPerformanceV126.js',
+  './uiPerformanceV127.js',
+  './dataLib.js',
+  './listeningStats.js',
+  './listeningStatsV81.js',
+  './listeningFixtures.js',
+  './listeningFixturesV99.js',
+  './listeningIdentityContracts.js',
+  './listeningDerivedStorage.js',
+  './listeningDerivedMigration.js',
+  './spotifyHistoryImport.js',
+  './listeningBandActivity.js',
+  './listeningVaultBridge.js',
+  './listeningVault.js',
+  './listeningHistoryV2.js',
+  './listeningIncrementalVault.js',
+  './listenbrainzSync.js',
+  './spotifyHistoryBootstrap.js',
+  './icons.js',
+  './conflictMerge.js',
+  './remoteStore.js',
+  './ownedTickets.js',
+  './musicbrainzState.js',
+  './providerIdentityState.js',
+  './weather.js',
+  './spotifyUser.js',
+  './spotifyUserV100.js',
+  './version.js',
+  './manifest.json',
+  './icons/bandmarkr-wordmark.svg',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-192-maskable.png',
+  './icons/icon-512-maskable.png',
+  './assets/listening/album-blue.svg',
+  './assets/listening/album-purple.svg',
+  './assets/listening/album-cyan.svg',
+  './assets/listening/album-gold.svg',
 ];
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => Promise.all(SHELL_FILES.map((url) => fetch(url, { cache: 'reload' }).then((res) => cache.put(url, res))))));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      Promise.all(SHELL_FILES.map((url) => fetch(url, { cache: 'reload' }).then((res) => cache.put(url, res))))
+    )
+  );
   self.skipWaiting();
 });
-self.addEventListener('message', (event) => { if (event.data?.type === 'SKIP_WAITING') self.skipWaiting(); });
+self.addEventListener('message', (event) => {
+  if (event.data?.type === 'SKIP_WAITING') self.skipWaiting();
+});
 self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k.startsWith('concert-tracker-shell-') && k !== CACHE_NAME).map((k) => caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k.startsWith('concert-tracker-shell-') && k !== CACHE_NAME).map((k) => caches.delete(k))))
+  );
   self.clients.claim();
 });
 self.addEventListener('fetch', (event) => {
