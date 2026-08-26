5ef06d6583dbf95ff20e021695cffbc7cc2b465d
mstpln <mattias.bjerstedt@gmail.com>
2026-08-26 16:12:40 +0200

    Bump service worker cache to v168

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
 'use strict';
 importScripts('./version.js');
-const CACHE_NAME_LITERAL = 'v167';
+const CACHE_NAME_LITERAL = 'v168';
+// Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v167'.
 // Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v166'.
 // Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v165'.
 // Previous merged release marker retained for regression coverage: CACHE_NAME_LITERAL = 'v164'.
